dc0b912fc998bc9e7edafbca9377346408fa130e
robin <hej@robin.se>
2025-01-08 19:05:55 +0000

    Added SF <3

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,17 @@ import Manifest from "./views/Manifest.vue"
 
 <template>
     <div class="container p-8 mx-auto">
-        <h1 class="text-lg font-light border-b dark:border-zinc-700 pb-4 mb-8">
+        <h1
+            class="flex items-center text-lg font-light border-b dark:border-zinc-700 pb-4 mb-8"
+        >
             Manifest Announcements
+
+            <a
+                href="https://skydive.se"
+                class="ml-auto font-medium text-sm opacity-50 hover:opacity-100"
+            >
+                💙 SF
+            </a>
         </h1>
 
         <Manifest />
